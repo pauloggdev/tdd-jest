aafd503558526aaf41c1552639f820dbfa2aefcd
pauloggdev <pauloggjoao@gmail.com>
2023-02-07 15:43:28 +0100

    adicionado arquivo de test

---
+++ index.js
@@ -0,0 +1,7 @@
+const sum = (n1, n2) => {
+    return n1 + n2
+}
+const sub = (n1, n2) => {
+    return n1 - n2
+}
+module.exports = { sum, sub };
---
+++ main.test.js
@@ -0,0 +1,8 @@
+const { sum, sub } = require('./index')
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+});
+test('adds 5 - 3 to equal 2', () => {
+    expect(sub(5, 3)).toBe(2);
+});
